f3f8c8eafba0916661fc3bb4c6580bee9b059683
eeliezer <eliezer_job@hotmail.com>
2022-10-23 20:00:33 -0300

    Añadir mas elementos al array

--- assets/main.js
+++ assets/main.js
@@ -47,7 +47,42 @@ const pizzasLista = [
 		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Cebolla Caramelizada"], 
 		precio: 950,	
 		imagen : "./assets/img/Pizza-Fugazzeta--300x300.jpg"	
-	}
+	},
+	{
+		id: 7,
+		nombre: "Pizza Napolitana",
+		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Aceitunas", "Tomates"],
+		precio: 500,
+		imagen: "./assets/img/Pizza-Napolitana--300x300.jpg"
+	},
+	{
+		id: 8,
+		nombre: "Panceta",
+		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Panceta", "Aceitunas"], 
+		precio: 1000,
+		imagen : "./assets/img/Pizza-Panceta--300x300.jpg"
+	},
+	{
+		id: 9,
+		nombre: "Roquefort",
+		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Roquefort", "Aceitunas"], 
+		precio: 650,		
+		imagen : "./assets/img/Pizza-Roquefort--300x300.jpg"
+	},
+	{
+		id: 10,
+		nombre: "Provolone",
+		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Provolone", "Aceitunas"], 
+		precio: 750,
+		imagen : "./assets/img/Pizza-Provolone-300x300.jpg.jpg"		
+	},
+	{
+		id: 11,
+		nombre: "Super Calabresa",
+		ingredientes: ["Salsa de Tomate", "Queso Muzzarella", "Calabresa", "Aceitunas Negras"], 
+		precio: 1550,
+		imagen : "./assets/img/Pizza-Super-Calabresa--300x300.jpg"		
+	},
 ];
 
 
